3e0b6796834b77ab9c68916860b9ebd72b123a3b
aermin <aermin@foxmail.com>
2019-05-18 16:45:52 +0800

    make a tag target as undefind as default

--- src/decorators/defaultComponentDecorator.jsx
+++ src/decorators/defaultComponentDecorator.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 
 export default (decoratedHref: string, decoratedText: string, key: number, openInNewWindow: boolean): React.Node => {
   return (
-    <a href={decoratedHref} key={key} target={ openInNewWindow ? "_blank" : '_self'}>
+    <a href={decoratedHref} key={key} target={ openInNewWindow ? "_blank" : undefined}>
       {decoratedText}
     </a>
   );
